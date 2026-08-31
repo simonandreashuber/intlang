1b08c368c22c80ca89d08209b393a95c048d26c7
Simon Huber <hubersim@ethz.ch>
2026-08-31 09:52:27 +0200

    lib frontend and backend subfolder

--- lib/errors.ml
+++ lib/errors.ml
@@ -1,3 +1,5 @@
+
+(*Frontend*)
 exception ParseError of string
 exception IncludeError of string
 exception RecCheckError of string
@@ -5,8 +7,11 @@ exception TypeError of string
 exception VecCheckError of string
 exception InterpError of string
 exception LowerMonoTASTError of string
+exception PrintError of string
+
+(*Mir*)
 exception MirError of string
 exception MirSimError of string
 
+(*Codegen*)
 exception CodegenError of string
-exception PrintError of string
